70de14643609559e9ff6a4f92d5a45feee773871
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2021-12-06 12:45:01 +0900

    再生回数やいいね回数による並び替えに対応

--- volumes/app/app.py
+++ volumes/app/app.py
@@ -11,26 +11,33 @@ def hello():
 @app.route('/search')
 def search():
     query = request.args.get("query")
+    sort = request.args.get("sort")
     con = my.connect(host="db", database="viblioteca", user="viblioteca", password="viblioteca")
     try:
         cur = con.cursor()
         try:
-            cur.execute("select id, org, title, description, width, height, "
-                "type, url, embeddable, thumbUrl, indexText from contents"
+            q = "select id, org, title, description, width, height, " \
+                "type, url, embeddable, thumbUrl, indexText, viewCount, likeCount from contents" \
                 " where LOWER(indexText) like %s"
-                " order by id", ("%" + query.lower() + "%",))
+            if sort == "viewCount":
+                q += " order by viewCount desc"
+            elif sort == "likeCount":
+                q += " order by likeCount desc"
+            else:
+                q += " order by id"
+            cur.execute(q, ("%" + query.lower() + "%",))
             result = list()
             for r in cur:
                 result.append({"id": r[0], "org": r[1], "title": r[2],
                     "description": r[3], "width": r[4], "height": r[5],
                     "type": r[6], "url": r[7], "embeddable": r[8] != 0,
-                    "thumbUrl": r[9], "indexText": r[10]})
+                    "thumbUrl": r[9], "indexText": r[10],
+                    "viewCount": r[11], "likeCount": r[12]})
             return jsonify(result)
         finally:
             cur.close()
     finally:
         con.close()
-#https://i.vimeocdn.com/video/998078192.webp?mw=900&mh=507
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
